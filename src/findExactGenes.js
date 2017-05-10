55f198ca830aff51500c720d7546c2f859267a90
oprogramador <uomodislesia@gmail.com>
2017-05-10 04:07:36 +0200

    findExactGenes optimization

--- src/findExactGenes.js
+++ src/findExactGenes.js
@@ -19,31 +19,46 @@ function findStart(first, second, positionAtFirst, positionAtSecond) {
   };
 }
 
+function findEnd(first, second, positionAtFirst, positionAtSecond) {
+  let shift = 0;
+  while (
+    first[positionAtFirst + 1] === second[positionAtSecond + 1] &&
+    first[positionAtFirst + 1] &&
+    second[positionAtSecond + 1]
+  ) {
+    positionAtFirst++;
+    positionAtSecond++;
+    shift++;
+  }
+
+  return {
+    positionAtFirst,
+    positionAtSecond,
+    shift,
+  };
+}
 function findExactGenes(first, second, { maxTimes = 600 } = {}) {
   const sequences = {};
 
   _.times(maxTimes, () => {
-    let sequenceLength = 20;
-    const start = _.random(first.sequence.length - sequenceLength);
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      const sequenceToSearch = first.sequence.substr(start, sequenceLength);
-      if (sequenceToSearch.includes('N')) {
-        break;
-      }
-      sequenceLength++;
-      const position = second.sequence.search(sequenceToSearch);
-      if (position >= 0) {
-        const { positionAtFirst, positionAtSecond, shift } =
-          findStart(first.sequence, second.sequence, start, position);
-        sequences[positionAtFirst] = {
-          positionAtFirst,
-          positionAtSecond,
-          sequence: first.sequence.substr(positionAtFirst, sequenceToSearch.length + shift),
-        };
-      } else {
-        break;
-      }
+    const initialLength = 20;
+    const start = _.random(first.sequence.length - initialLength);
+    const sequenceToSearch = first.sequence.substr(start, initialLength);
+    if (sequenceToSearch.includes('N')) {
+      return;
+    }
+    const position = second.sequence.search(sequenceToSearch);
+    if (position >= 0) {
+      const foundStart = findStart(first.sequence, second.sequence, start, position);
+      const foundEnd = findEnd(first.sequence, second.sequence, start + initialLength, position + initialLength);
+      sequences[foundStart.positionAtFirst] = {
+        positionAtFirst: foundStart.positionAtFirst,
+        positionAtSecond: foundStart.positionAtSecond,
+        sequence: first.sequence.substr(
+          foundStart.positionAtFirst,
+          sequenceToSearch.length + foundStart.shift + foundEnd.shift
+        ),
+      };
     }
   });
 
