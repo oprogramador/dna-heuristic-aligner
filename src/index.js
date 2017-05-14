098428db6c13fceaa243625dce7a0e8006f772e8
oprogramador <uomodislesia@gmail.com>
2017-05-14 20:44:39 +0200

    use groupedSequences

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import findExactGenes from 'dna-heuristic-aligner/findExactGenes';
 import fs from 'fs';
+import groupClosestSequences from 'dna-heuristic-aligner/groupClosestSequences';
 import parseFNA from 'fna-parser';
 import sumSequencesLength from 'dna-heuristic-aligner/measurers/sumSequencesLength';
 
 const first = parseFNA(fs.readFileSync(`${__dirname}/../data/hs_alt_CHM1_1.1_chr1.fa`).toString())[0];
 const second = parseFNA(fs.readFileSync(`${__dirname}/../data/9595_ref_gorGor4_chr1.fa`).toString())[0];
 
-const sequences = findExactGenes(first, second, { maxTimes: 20 });
+const sequences = findExactGenes(first, second, { maxTimes: 200 });
+const groupedSequences = groupClosestSequences(first.sequence, second.sequence, sequences, 1000);
 
 /* eslint-disable no-console */
 console.log(`Sequences length sum: ${sumSequencesLength(sequences)}`);
-console.log(sequences);
+console.log(groupedSequences);
