a470e16a3d01eff997f8aa6128c2ca82a1e9f8da
oprogramador <uomodislesia@gmail.com>
2017-06-15 11:46:08 +0200

    save in arango

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import fs from 'fs';
 import groupClosestSequences from 'dna-heuristic-aligner/groupClosestSequences';
 import logger from 'dna-heuristic-aligner/services/logger';
 import parseFNA from 'fna-parser';
+import saveInArango from 'dna-heuristic-aligner/storage/saveInArango';
 import sumSequencesLength from 'dna-heuristic-aligner/measurers/sumSequencesLength';
 
 const first = parseFNA(fs.readFileSync(`${__dirname}/../data/hs_alt_CHM1_1.1_chr1.fa`).toString())[0];
 const second = parseFNA(fs.readFileSync(`${__dirname}/../data/9595_ref_gorGor4_chr1.fa`).toString())[0];
 
-const sequences = findExactGenes(first, second, { maxTimes: 2000 });
+const sequences = findExactGenes(first, second, { maxTimes: 1000 });
 const groupedSequences = groupClosestSequences(first.sequence, second.sequence, sequences, 10);
-const mutatedSequences = _.filter(groupedSequences, group => group.sequenceAtFirst !== group.sequenceAtSecond);
 
 /* eslint-disable no-console */
 logger.info(`Sequences length sum: ${sumSequencesLength(sequences)}`);
-logger.info(mutatedSequences);
+
+const mutatedSequences = _.filter(groupedSequences, (group, key) => {
+  delete groupedSequences[key];
+
+  return group.sequenceAtFirst !== group.sequenceAtSecond;
+});
+saveInArango(mutatedSequences);
