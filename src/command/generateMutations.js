3776adaeea31ceee8c989b0ea46bcbd698b38245
oprogramador <uomodislesia@gmail.com>
2017-07-05 01:55:58 +0200

    generate mutations in FASTA format

--- src/command/generateMutations.js
+++ src/command/generateMutations.js
@@ -1,13 +1,15 @@
+import parseFASTA, { writeFASTA } from 'fasta-to-object-parser';
 import fs from 'fs';
 import generateMutations from 'dna-heuristic-aligner/generation/generateMutations';
-import parseFASTA from 'fasta-to-object-parser';
 import process from 'process';
 
 const sequenceName = process.argv[2];
 const count = process.argv[3];
 
-const sequence = parseFASTA(fs.readFileSync(sequenceName).toString())[0].sequence;
-const newSequence = generateMutations(sequence, count);
+const mutated = writeFASTA(
+  parseFASTA(fs.readFileSync(sequenceName).toString())
+    .map(dna => Object.assign({}, dna, { sequence: generateMutations(dna.sequence, count) }))
+);
 
 // eslint-disable-next-line no-console
-console.log(newSequence);
+console.log(mutated);
