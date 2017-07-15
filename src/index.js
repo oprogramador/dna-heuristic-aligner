cad9bafdaf97a5a736b4128b47db0d3d8b4b7913
oprogramador <uomodislesia@gmail.com>
2017-07-15 14:11:08 +0200

    update to newest grapedb

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+import { AdvancedManager, LevelSimpleManager } from 'grapedb';
+import LevelPromise from 'level-promise';
 import _ from 'lodash';
 import findMutationsInsideAlignment from 'dna-heuristic-aligner/findMutationsInsideAlignment';
 import findMutationsWithExtendingAndLookingNear from 'dna-heuristic-aligner/findMutationsWithExtendingAndLookingNear';
 import findMutationsWithJoining from 'dna-heuristic-aligner/findMutationsWithJoining';
 import findMutationsWithOnlyExtending from 'dna-heuristic-aligner/findMutationsWithOnlyExtending';
 import fs from 'fs';
+import levelup from 'levelup';
 import logger from 'dna-heuristic-aligner/services/logger';
 import parseFASTA from 'fasta-to-object-parser';
 import process from 'process';
-import { saveInLevelDB } from 'grapedb';
 import stringSimilarity from 'string-similarity';
 import sumSimilarity from 'dna-heuristic-aligner/measurers/sumSimilarity';
 
@@ -50,9 +52,12 @@ const additionalInfo = {
   updated: new Date().toISOString(),
 };
 const toSave = { additionalInfo, allExactMutations, mutatedSequences };
-saveInLevelDB(`${__dirname}/../leveldb`, logger)(additionalInfo.updated, toSave)
+const db = LevelPromise(levelup(`${__dirname}/../leveldb`));
+new AdvancedManager(new LevelSimpleManager(db), logger).set(additionalInfo.updated, toSave)
+  .then(() => db.close())
   .then(() => {
     const similarity = sumSimilarity(mutatedSequences, stringSimilarity.compareTwoStrings);
+    logger.info(`saved as ${additionalInfo.updated}`);
     logger.info({ similarity });
     logger.info(`Found ${Object.keys(allExactMutations).length} mutations in ${_.size(mutatedSequences)} sequences.`);
   });
