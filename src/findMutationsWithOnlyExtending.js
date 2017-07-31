1c1dfeaf0209ae79e716a7aa34d4070d8f2e9860
oprogramador <uomodislesia@gmail.com>
2017-07-31 09:59:14 +0200

    iterate with bluebird

--- src/findMutationsWithOnlyExtending.js
+++ src/findMutationsWithOnlyExtending.js
@@ -1,5 +1,5 @@
 import { findEnd, findStart } from 'dna-heuristic-aligner/extending';
-import _ from 'lodash';
+import bluebird from 'bluebird';
 import findMutationsInsideAlignment from 'dna-heuristic-aligner/findMutationsInsideAlignment';
 import logger from 'dna-heuristic-aligner/services/logger';
 
@@ -18,69 +18,68 @@ const generateRandomInteger = (() => {
 })();
 
 function findMutationsWithOnlyExtending(first, second, { manager, mainKey, rootKey }) {
-  const maxTimes = first.length / initialLength;
-  const savedKeys = new Set();
-  const promises = [
-    manager.getComplex(rootKey, 2)
-      .then(root => manager.setComplex(rootKey, [...(root || []), mainKey])),
-  ];
+  const maxTimes = Math.ceil(first.length / initialLength);
   const toSave = {};
 
-  _.times(maxTimes, (i) => {
-    if (i % 100 === 0) {
-      logger.info({ i, maxTimes });
-    }
-    const start = generateRandomInteger() % first.length;
-    const sequenceToSearch = first.substr(start, initialLength);
-    if (sequenceToSearch.includes('N')) {
-      return;
-    }
-    const position = second.search(sequenceToSearch);
-    if (position >= 0) {
-      const foundStart = findStart(first, second, start, position);
-      const initialShift = initialLength - 1;
-      const foundEnd = findEnd(first, second, start + initialShift, position + initialShift);
-      const foundSequence = {
-        positionAtFirst: foundStart.positionAtFirst,
-        positionAtSecond: foundStart.positionAtSecond,
-        sequenceAtFirst: first.substr(
-          foundStart.positionAtFirst,
-          sequenceToSearch.length + foundStart.shift + foundEnd.shift
-        ),
-        sequenceAtSecond: second.substr(
-          foundStart.positionAtSecond,
-          sequenceToSearch.length + foundStart.shift + foundEnd.shift
-        ),
-      };
-
-      const reversedFirst = foundSequence.sequenceAtFirst.split('').reverse().join('');
-      const reversedSecond = foundSequence.sequenceAtSecond.split('').reverse().join('');
-      const leftDiffShift = reversedFirst.indexOf(reversedSecond.substr(0, initialLength));
-      const diffShift = leftDiffShift >= 0
-        ? leftDiffShift
-        : -reversedSecond.indexOf(reversedFirst.substr(0, initialLength));
-
-      foundSequence.sequenceAtSecond = second.substr(
-        foundStart.positionAtSecond,
-        sequenceToSearch.length + foundStart.shift + foundEnd.shift + diffShift
-      );
-
-      if (
-        foundSequence.sequenceAtFirst.length >= initialLength &&
-        foundSequence.sequenceAtFirst !== foundSequence.sequenceAtSecond
-      ) {
-        const mutations = findMutationsInsideAlignment(foundSequence);
-        foundSequence.mutations = mutations;
-        const key = foundStart.positionAtFirst;
-        toSave[key] = foundSequence;
-        savedKeys.add(key);
-        promises.push(manager.setComplex(key, foundSequence));
+  return manager.getComplex(rootKey, 2)
+    .then(root => manager.setComplex(rootKey, [...(root || []), mainKey]))
+    .then(() => bluebird.reduce(new Array(maxTimes), (accumulator, nothing, i) => {
+      if (i % 100 === 0) {
+        logger.info({ i, maxTimes });
       }
-    }
-  });
-  promises.push(manager.setComplex(mainKey, toSave));
+      const start = generateRandomInteger() % first.length;
+      const sequenceToSearch = first.substr(start, initialLength);
+      if (sequenceToSearch.includes('N')) {
+        return Promise.resolve();
+      }
+      const position = second.search(sequenceToSearch);
+      if (position >= 0) {
+        const foundStart = findStart(first, second, start, position);
+        const initialShift = initialLength - 1;
+        const foundEnd = findEnd(first, second, start + initialShift, position + initialShift);
+        const foundSequence = {
+          positionAtFirst: foundStart.positionAtFirst,
+          positionAtSecond: foundStart.positionAtSecond,
+          sequenceAtFirst: first.substr(
+            foundStart.positionAtFirst,
+            sequenceToSearch.length + foundStart.shift + foundEnd.shift
+          ),
+          sequenceAtSecond: second.substr(
+            foundStart.positionAtSecond,
+            sequenceToSearch.length + foundStart.shift + foundEnd.shift
+          ),
+        };
 
-  return Promise.all(promises);
+        const reversedFirst = foundSequence.sequenceAtFirst.split('').reverse().join('');
+        const reversedSecond = foundSequence.sequenceAtSecond.split('').reverse().join('');
+        const leftDiffShift = reversedFirst.indexOf(reversedSecond.substr(0, initialLength));
+        const diffShift = leftDiffShift >= 0
+          ? leftDiffShift
+          : -reversedSecond.indexOf(reversedFirst.substr(0, initialLength));
+
+        foundSequence.sequenceAtSecond = second.substr(
+          foundStart.positionAtSecond,
+          sequenceToSearch.length + foundStart.shift + foundEnd.shift + diffShift
+        );
+
+        if (
+          foundSequence.sequenceAtFirst.length >= initialLength &&
+          foundSequence.sequenceAtFirst !== foundSequence.sequenceAtSecond
+        ) {
+          const mutations = findMutationsInsideAlignment(foundSequence);
+          foundSequence.mutations = mutations;
+          const key = foundStart.positionAtFirst;
+          toSave[key] = foundSequence;
+
+          return manager.setComplex(key, foundSequence);
+        }
+
+        return Promise.resolve();
+      }
+
+      return Promise.resolve();
+    }))
+    .then(() => manager.setComplex(mainKey, toSave));
 }
 
 export default findMutationsWithOnlyExtending;
