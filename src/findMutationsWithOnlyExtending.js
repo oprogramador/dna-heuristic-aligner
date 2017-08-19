4b97166e1e26568512c4578891b85e984bd81bad
oprogramador <uomodislesia@gmail.com>
2017-08-19 17:37:59 +0200

    async/wait instead of bluebird

--- src/findMutationsWithOnlyExtending.js
+++ src/findMutationsWithOnlyExtending.js
@@ -1,5 +1,4 @@
 import { findEnd, findStart } from 'dna-heuristic-aligner/extending';
-import bluebird from 'bluebird';
 import findMutationsInsideAlignment from 'dna-heuristic-aligner/findMutationsInsideAlignment';
 import logger from 'dna-heuristic-aligner/services/logger';
 
@@ -10,10 +9,7 @@ const generateRandomInteger = i => i * initialLength;
 const createCurrentProcessKey = (firstSource, secondSource) =>
   `current-process-${JSON.stringify({ firstSource, secondSource })}`;
 
-const executeSequentially = (maxTimes, callback) =>
-  bluebird.reduce(new Array(maxTimes), (accumulator, nothing, i) => callback(i));
-
-function findMutationsWithOnlyExtending(
+async function findMutationsWithOnlyExtending(
   first,
   second,
   {
@@ -28,74 +24,66 @@ function findMutationsWithOnlyExtending(
   const currentProcessKey = createCurrentProcessKey(firstSource, secondSource);
   logger.info({ currentProcessKey });
 
-  return Promise.all([
+  const [root, currentProcess] = await Promise.all([
     manager.getComplex(rootKey, 2),
     manager.getComplex(currentProcessKey),
-  ])
-    .then(([root, currentProcess]) => {
-      const { iterationNr, mainKey } = currentProcess || { iterationNr: 0, mainKey: defaultMainKey };
-
-      return manager.setComplex(rootKey, [...(root || []), mainKey])
-        .then(() => ({ iterationNr, mainKey }));
-    })
-    .then(({ iterationNr, mainKey }) => executeSequentially(maxTimes - iterationNr, (i) => {
-      const currentIteration = iterationNr + i;
-      if (currentIteration % 100 === 0) {
-        logger.info({ firstSource, i: currentIteration, maxTimes, secondSource });
-        manager.setComplex(currentProcessKey, { iterationNr: currentIteration, mainKey });
-      }
-      const start = generateRandomInteger(currentIteration) % first.length;
-      const sequenceToSearch = first.substr(start, initialLength);
-      if (sequenceToSearch.includes('N')) {
-        return Promise.resolve();
-      }
-      const position = second.search(sequenceToSearch);
-      if (position >= 0) {
-        const foundStart = findStart(first, second, start, position);
-        const initialShift = initialLength - 1;
-        const foundEnd = findEnd(first, second, start + initialShift, position + initialShift);
-        const foundSequence = {
-          positionAtFirst: foundStart.positionAtFirst,
-          positionAtSecond: foundStart.positionAtSecond,
-          sequenceAtFirst: first.substr(
-            foundStart.positionAtFirst,
-            sequenceToSearch.length + foundStart.shift + foundEnd.shift
-          ),
-          sequenceAtSecond: second.substr(
-            foundStart.positionAtSecond,
-            sequenceToSearch.length + foundStart.shift + foundEnd.shift
-          ),
-        };
-
-        const reversedFirst = foundSequence.sequenceAtFirst.split('').reverse().join('');
-        const reversedSecond = foundSequence.sequenceAtSecond.split('').reverse().join('');
-        const leftDiffShift = reversedFirst.indexOf(reversedSecond.substr(0, initialLength));
-        const diffShift = leftDiffShift >= 0
-          ? leftDiffShift
-          : -reversedSecond.indexOf(reversedFirst.substr(0, initialLength));
-
-        foundSequence.sequenceAtSecond = second.substr(
+  ]);
+  const { iterationNr, mainKey } = currentProcess || { iterationNr: 0, mainKey: defaultMainKey };
+  await manager.setComplex(rootKey, [...(root || []), mainKey]);
+  for (let currentIteration = iterationNr + 1; currentIteration < maxTimes; currentIteration++) {
+    if (currentIteration % 100 === 0) {
+      logger.info({ firstSource, i: currentIteration, maxTimes, secondSource });
+      manager.setComplex(currentProcessKey, { iterationNr: currentIteration, mainKey });
+    }
+    const start = generateRandomInteger(currentIteration) % first.length;
+    const sequenceToSearch = first.substr(start, initialLength);
+    if (sequenceToSearch.includes('N')) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    const position = second.search(sequenceToSearch);
+    if (position >= 0) {
+      const foundStart = findStart(first, second, start, position);
+      const initialShift = initialLength - 1;
+      const foundEnd = findEnd(first, second, start + initialShift, position + initialShift);
+      const foundSequence = {
+        positionAtFirst: foundStart.positionAtFirst,
+        positionAtSecond: foundStart.positionAtSecond,
+        sequenceAtFirst: first.substr(
+          foundStart.positionAtFirst,
+          sequenceToSearch.length + foundStart.shift + foundEnd.shift
+        ),
+        sequenceAtSecond: second.substr(
           foundStart.positionAtSecond,
-          sequenceToSearch.length + foundStart.shift + foundEnd.shift + diffShift
-        );
+          sequenceToSearch.length + foundStart.shift + foundEnd.shift
+        ),
+      };
 
-        if (
-          foundSequence.sequenceAtFirst.length >= initialLength &&
-          foundSequence.sequenceAtFirst !== foundSequence.sequenceAtSecond
-        ) {
-          const mutations = findMutationsInsideAlignment(foundSequence);
-          foundSequence.mutations = mutations;
-          const key = foundStart.positionAtFirst;
+      const reversedFirst = foundSequence.sequenceAtFirst.split('').reverse().join('');
+      const reversedSecond = foundSequence.sequenceAtSecond.split('').reverse().join('');
+      const leftDiffShift = reversedFirst.indexOf(reversedSecond.substr(0, initialLength));
+      const diffShift = leftDiffShift >= 0
+        ? leftDiffShift
+        : -reversedSecond.indexOf(reversedFirst.substr(0, initialLength));
 
-          return manager.getComplex(mainKey, 1)
-            .then(result => manager.setComplex(mainKey, Object.assign({}, result, { [key]: foundSequence })));
-        }
+      foundSequence.sequenceAtSecond = second.substr(
+        foundStart.positionAtSecond,
+        sequenceToSearch.length + foundStart.shift + foundEnd.shift + diffShift
+      );
 
-        return Promise.resolve();
+      if (
+        foundSequence.sequenceAtFirst.length >= initialLength &&
+        foundSequence.sequenceAtFirst !== foundSequence.sequenceAtSecond
+      ) {
+        const mutations = findMutationsInsideAlignment(foundSequence);
+        foundSequence.mutations = mutations;
+        const key = foundStart.positionAtFirst;
+
+        const result = await manager.getComplex(mainKey, 1);
+        await manager.setComplex(mainKey, Object.assign({}, result, { [key]: foundSequence }));
       }
-
-      return Promise.resolve();
-    }));
+    }
+  }
 }
 
 export default findMutationsWithOnlyExtending;
