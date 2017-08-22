ef059bc06065f03e6d8f46715a5bc85268c47416
oprogramador <uomodislesia@gmail.com>
2017-08-22 10:25:32 +0200

    remove sumSimilarity

--- src/measurers/sumSimilarity.js
+++
@@ -1,7 +0,0 @@
-import _ from 'lodash';
-
-function sumSimilarity(data, getSimilarity) {
-  return _.sum(_.values(data).map(value => getSimilarity(value.sequenceAtFirst, value.sequenceAtSecond)));
-}
-
-export default sumSimilarity;
