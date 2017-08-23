0389187569648241d2413c83c99ee8a5be8a6dba
oprogramador <uomodislesia@gmail.com>
2017-08-23 11:28:23 +0200

    read allMutationsIndexesAtFirstSequence

--- src/command/readFromDatabase.js
+++ src/command/readFromDatabase.js
@@ -1,8 +1,10 @@
 import { AdvancedManager, LevelSimpleManager } from 'grapedb';
 import LevelPromise from 'level-promise';
+import _ from 'lodash';
 import levelup from 'levelup';
 
 const key = process.argv[2];
+const way = process.argv[3];
 const leveldbDir = process.env.DNA_LEVELDB_DIR;
 
 const logger = {
@@ -13,6 +15,15 @@ const db = LevelPromise(levelup(leveldbDir));
 const manager = new AdvancedManager(new LevelSimpleManager(db), logger);
 
 (async () => {
+  const result = await (async () => {
+    const data = await manager.getComplex(key);
+    if (way === 'allMutationsIndexesAtFirstSequence') {
+      return _.flatten(_.values(data).map(alignment => Object.keys(alignment.mutations || [])));
+    }
+
+    return data;
+  })();
+
   // eslint-disable-next-line no-console
-  console.log(JSON.stringify(await manager.getComplex(key)));
+  console.log(JSON.stringify(result));
 })();
